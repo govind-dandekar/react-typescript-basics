7d21254507bb7e58658b7f611dc8e9175329fbaf
Govind Dandekar <govind.dandekar@gmail.com>
2025-01-23 12:08:58 -0800

    review TypeScript generics

--- basics.ts
+++ basics.ts
@@ -18,11 +18,14 @@ let hobbies: string[];
 
 hobbies = ['Sports', 'Reading'];
 
-// objects
-let person: {
+// type aliases; not JS value.  is a type definition
+type Person = {
 	name: string;
 	age: number;
-}; 
+}
+
+// objects
+let person: Person;
 
 person = {
 	name: 'max',
@@ -30,10 +33,7 @@ person = {
 }
 
 // array of objects
-let people: {
-	name: string;
-	age: number;
-}[];
+let people: Person[];
 
 // type inference - ts infers type of course variable w/o explicit declaration
 // if variable initialized with value, ts will use value type as inferred type
@@ -45,6 +45,47 @@ let people: {
 let course: string | number = 'React - the complete guide';
 
 course = 12345;
+
+// fx and types
+
+function addition(a: number, b: number) {
+	// returned value (number) is inferred
+	return a + b;
+};
+
+function printOutput(value: any){
+	console.log(value);
+}
+
+// generics: <T> are ts feature
+// can use T type in function
+// when fx called with generics, ts knows it should look
+// at concrete argument types and will return numnber[], string[],
+// et al.  T means type of array and value will be the SAME T type
+function insertAtBeginning<T>(array: T[], value: T){
+	const newArray = [value, ...array];
+	// utility fx that returns new array
+	return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+// inferred to by any; ts will not infer numbers due to initial typing
+// may need to use with string[]
+// will not get string support even if a string array because
+// assumed to be any[] (not string[])
+const updatedArray = insertAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+
+const stringArray = insertAtBeginning(['a', 'b', 'c'], 'd');
+
+
+
+
+
+
+
+
+
 
 
 
